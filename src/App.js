445c5f6e5f71e6421a6c2fe69b652eb02dde92e3
Karthik Prakash <karthikp.ivy@gmail.com>
2022-12-29 16:00:05 +0530

    Checking Netlify secrets

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          process.env.SOME_SECRET
+          {process.env.SOME_SECRET}
         </p>
         <a
           className="App-link"
