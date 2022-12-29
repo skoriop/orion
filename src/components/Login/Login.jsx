3f3da46a50638a48cb2fa39d2faf85f795dc07de
Karthik Prakash <karthikp.ivy@gmail.com>
2022-12-30 01:35:07 +0530

    Expanded user profile

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 function Login() {
     const clientID = '006d8bf0179d4861a7ccc8d121768c68';
     const redirectURI =  window.location.origin + window.location.pathname;
+    const scopes = 'user-library-read user-read-recently-played user-top-read user-read-currently-playing user-follow-read playlist-read-private user-read-private user-read-email';
+    var url = 'https://accounts.spotify.com/authorize';
+    url += '?response_type=token';
+    url += `&client_id=${clientID}`;
+    url += `&redirect_uri=${redirectURI}`;
+    url += `&scope=${encodeURIComponent(scopes)}`;
     return (
         <button>
-            <a href={'https://accounts.spotify.com/authorize?client_id='+clientID+'&redirect_uri='+redirectURI+'&response_type=token'}>
+            <a href={url}>
                 Login to Spotify
             </a>
         </button>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,14 +7,17 @@ function Profile() {
     useEffect(() => {
         callAPI("GET", "me").then(d => setData(d));
     }, []);
-    console.log(data);
-    if (data === {}) return(<div></div>);
+    // console.log(JSON.stringify(data));
+    if (JSON.stringify(data).length === 2) return (<div></div>);
     else return (
         <div>
             <h2>
-                <a href={`${data.external_urls.spotify}`}>{data.display_name}</a>
+                <a href={data.external_urls.spotify}>{data.display_name}</a>
             </h2>
             <h4>{data.followers.total} followers</h4>
+            <p>Country: {data.country}</p>
+            <p>Email: {data.email}</p>
+            <p>Membership: {data.product}</p>
         </div>
     );
 }
